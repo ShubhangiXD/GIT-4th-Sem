4c0a29ba1c8b6b51093c374c07b9f53d53e2d2e2
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-02-27 08:40:05 +0530

    modified:   web dev/Personal/kohi.html 	new file:   web dev/Personal/kohistyle.css

---
+++ Java Personal/Extra/multimain.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class multimain {
+    public static void main() {
+        System.out.println("Main without String args");
+    }    
+    public static void main(String[] args) {
+        System.out.println("Proper main syntax");
+    }
+}
