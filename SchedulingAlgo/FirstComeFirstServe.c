447889c5799dbf5000ea748062de8664baf4023a
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-04-04 16:20:55 +0530

    modified:   SchedulingAlgo/FirstComeFirstServe.c

--- SchedulingAlgo/FirstComeFirstServe.c
+++ SchedulingAlgo/FirstComeFirstServe.c
@@ -3,14 +3,14 @@
 struct process
 {
     int pid;          // process ID
-    int arrival_time; // arrival time of process
-    int burst_time;   // burst time of process
+    int arr; // arrival time of process
+    int burst;   // burst time of process
 };
 
 int main()
 {
     int n, i, j;
-    float avg_wait_time = 0, avg_turnaround_time = 0;
+    float avgwt = 0, avgtat = 0;
     printf("Enter number of processes: ");
     scanf("%d", &n);
     struct process p[n], temp;
@@ -18,14 +18,14 @@ int main()
     for (i = 0; i < n; i++)
     {
         printf("Process %d: ", i + 1);
-        scanf("%d%d%d", &p[i].pid, &p[i].arrival_time, &p[i].burst_time);
+        scanf("%d%d%d", &p[i].pid, &p[i].arr, &p[i].burst);
     }
    
     for (i = 0; i < n - 1; i++)
     {
         for (j = 0; j < n - i - 1; j++)
         {
-            if (p[j].arrival_time > p[j + 1].arrival_time)
+            if (p[j].arr > p[j + 1].arr)
             {
                 temp = p[j];
                 p[j] = p[j + 1];
@@ -34,26 +34,26 @@ int main()
         }
     }
     
-    int current_time = 0;
+    int curr = 0;
     for (i = 0; i < n; i++)
     {
-        if (current_time < p[i].arrival_time)
+        if (curr < p[i].arr)
         {
-            current_time = p[i].arrival_time;
+            curr = p[i].arr;
         }
-        avg_wait_time += current_time - p[i].arrival_time;
-        avg_turnaround_time += current_time - p[i].arrival_time + p[i].burst_time;
-        current_time += p[i].burst_time;
+        avgwt += curr - p[i].arr;
+        avgtat += curr - p[i].arr + p[i].burst;
+        curr += p[i].burst;
     }
   
-    avg_wait_time /= n;
-    avg_turnaround_time /= n;
+    avgwt /= n;
+    avgtat /= n;
     printf("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++)
     {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i].pid, p[i].arrival_time, p[i].burst_time, current_time - p[i].burst_time - p[i].arrival_time, current_time - p[i].arrival_time);
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i].pid, p[i].arr, p[i].burst, curr - p[i].burst - p[i].arr, curr - p[i].arr);
     }
-    printf("Average Waiting Time: %.2f\n", avg_wait_time);
-    printf("Average Turnaround Time: %.2f\n", avg_turnaround_time);
+    printf("Average Waiting Time: %.2f\n", avgwt);
+    printf("Average Turnaround Time: %.2f\n", avgtat);
     return 0;
 }
